778cf04bc14e2934603243bd26ccf54485a2f397
zhangxing <zxstarxing@gmail.com>
2019-07-12 19:24:56 +0800

    项目初始化

---
+++ src/components/Demo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Demo extends Component {
+  render() {
+    return (
+      <div>
+        这是一个组件
+      </div>
+    )
+  }
+}
---
+++ src/pages/Index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Demo from '../components/Demo';
+
+export default class Index extends Component {
+  render() {
+    return (
+      <Demo/>
+    )
+  }
+}
